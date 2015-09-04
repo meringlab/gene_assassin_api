412142cb6d507fb931a19d3fd94f5418925154d9
Milan Simonovic <mbsimonovic@gmail.com>
2015-09-04 16:58:51 +0200

    bugfix: docker run flags

--- api/load/proteins/build.sh
+++ api/load/proteins/build.sh
@@ -3,5 +3,5 @@
 sudo docker build -t crispr/mongo mongodb
 sudo docker build -t crispr/load load
 
-sudo docker run -d --name crispr_mongo -h mongodb crispr/mongo 
-sudo docker run  --name crispr_load --link crispr_mongo crispr/load
+sudo docker run -d -P --name crispr_mongo -h mongodb crispr/mongo 
+sudo docker run --rm  --name crispr_load --link crispr_mongo crispr/load
