65b723713311b6bff71c2a3ac15e1d31aa56abcd
Milan Simonovic <mbsimonovic@gmail.com>
2017-01-23 18:10:12 +0100

    different container name to avoid clashes

--- api/load/proteins/build.sh
+++ api/load/proteins/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo docker build -t crispr/mongo mongodb
-sudo docker build -t crispr/load load
+sudo docker build -t crispr/load_proteins load
 
 sudo docker run -d -P --name crispr_mongo -h mongodb crispr/mongo 
-sudo docker run --rm  --name crispr_load --link crispr_mongo crispr/load
+sudo docker run --rm  --name crispr_load_proteins --link crispr_mongo crispr/load_proteins
