fe02dc2d140171d1971b0c06c6cb6127e84d3d70
Milan Simonovic <mbsimonovic@gmail.com>
2017-01-25 16:13:39 +0100

    new version - more species (+refactor)

--- api/load/proteins/build.sh
+++ api/load/proteins/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-sudo docker build -t crispr/mongo mongodb
-sudo docker build -t crispr/load_proteins load
-
-sudo docker run -d -P --name crispr_mongo -h mongodb crispr/mongo 
+sudo docker build -t crispr/load_proteins .
 sudo docker run --rm  --name crispr_load_proteins --link crispr_mongo crispr/load_proteins
